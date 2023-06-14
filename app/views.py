6c090e24c8a60dd35603c10dad1fda1a2eb18c56
Ngoctrainguyen <nguyennhantamdan@gmail.com>
2023-06-14 23:54:55 +0700

    get database in homepage

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'app/home.html', context)
 
 def cart(request):
     return render(request, 'app/cart.html')
